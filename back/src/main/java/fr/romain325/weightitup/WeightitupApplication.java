834ff290faa431a0c72c13f00e6fa650c8e3bf99
romain325 <romainolivier42@gmail.com>
2022-10-24 18:49:37 +0200

    init spring

---
+++ back/src/main/java/fr/romain325/weightitup/WeightitupApplication.java
@@ -0,0 +1,13 @@
+package fr.romain325.weightitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightitupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeightitupApplication.class, args);
+    }
+
+}
